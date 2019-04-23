85a2b16b028e0e1a2196e4b494da5a3b0c414aac
DeveloperEmre <49831101+DeveloperEmre@users.noreply.github.com>
2019-04-23 17:14:08 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -381,4 +381,4 @@ client.on('error', e => {
   console.log(chalk.bgRed(e.replace(regToken, 'that was redacted')));
 });
 
-client.login(process.env.ayarlar.token);
+client.login(process.env.BOT_TOKEN);
